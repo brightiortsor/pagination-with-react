2340fccb68315c0ab10e3a3a87eaf2abd21a259e
Bright Iortsor <iortsor@gmail.com>
2022-10-25 10:36:58 +0100

    new component changes

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,28 +1,40 @@
 import React from "react";
-import Users from "./Users";
+import "../App.css";
 
-const Pagination = ({ totalPages, handlePrev, handleClick, handleNext, disabledPrev, disabledNext }) => {
+const Pagination = ({
+  totalPages,
+  handlePrev,
+  handleClick,
+  handleNext,
+  disabledPrev,
+  disabledNext,
+}) => {
   const pages = [...Array(totalPages).keys()].map((num) => num + 1);
   // const [page, setPage] = useState(1);
 
   return (
-    <div className="pagin">      
-        <button
-          disabled={disabledPrev}
-          aria-disabled={disabledPrev}
-          onClick={() => handlePrev()}>Prev</button>
-      <div>{pages.map((num) => (
-        <button key={num} onClick={() => handleClick(num)}>
+    <div className="pagin">
+      <button
+        disabled={disabledPrev}
+        aria-disabled={disabledPrev}
+        onClick={() => handlePrev()}
+      >
+        Prev
+      </button>
+
+      {pages.map((num) => (
+        <button key={num} onClick={() => handleClick(num)} className="btn-main">
           {num}
         </button>
-      ))}</div>
-        <button
-          disabled={disabledNext}
-          aria-disabled={disabledNext}
-          onClick={() => handleNext()}
-        >
-          Next
-        </button>
+      ))}
+
+      <button
+        disabled={disabledNext}
+        aria-disabled={disabledNext}
+        onClick={() => handleNext()}
+      >
+        Next
+      </button>
     </div>
   );
 };
---
+++ src/components/singleUser.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App.css";
+
+const SingleUser = () => {
+  return (
+    <div className="data">
+      <h1>User One Here</h1>
+    </div>
+  );
+};
+
+export default SingleUser;

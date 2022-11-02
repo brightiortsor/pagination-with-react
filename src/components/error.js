cba8bb91d3c7446cdcdafc3c4cbed050157c7c37
Bright Iortsor <iortsor@gmail.com>
2022-11-02 09:51:01 +0000

    error file updated

--- src/components/error.js
+++ src/components/error.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function error() {
   return (
     <div className="wrapper">
-      return <h1>Something went badly wrong!!!</h1>
+      <h1>Something went badly wrong!!!</h1>
     </div>
   );
 }
